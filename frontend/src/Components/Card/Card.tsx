0f790bd7df923dec538768e90b38d0ecc84c3608
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-08-19 23:21:46 +0300

    First commit

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,34 @@
+import "./Card.css";
+
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
+
+const Card: React.FC<Props> = ({
+  companyName,
+  ticker,
+  price,
+}: Props): JSX.Element => {
+  return (
+    <div className="card">
+      <img
+        src="https://images.unsplash.com/photo-1672307974995-cd253f7f7eeb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Image"
+      />
+      <div className="details">
+        <h2>
+          {companyName} ({ticker})
+        </h2>
+        <p>${price}</p>
+      </div>
+      <p className="info">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis,
+        tempora.
+      </p>
+    </div>
+  );
+};
+
+export default Card;
---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,22 @@
+import React, { ChangeEvent, useState, SyntheticEvent, FormEvent } from "react";
+
+interface Props {
+  onClick: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Search: React.FC<Props> = ({
+  onClick,
+  search,
+  handleChange,
+}: Props): JSX.Element => {
+  return (
+    <div>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)} />
+    </div>
+  );
+};
+
+export default Search;
