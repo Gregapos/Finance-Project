63d7fda35890c6bb578442e65487b77f2c86c1fe
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-08-29 14:49:00 +0300

    Add Entity Framework

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,21 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var connectionStringTemplate = builder.Configuration.GetConnectionString("DefaultConnection");
+var machineName = Environment.MachineName;
+var connectionString = connectionStringTemplate?.Replace("{ServerName}", machineName);
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
