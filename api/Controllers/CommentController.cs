8eb85e9f077ac4e58717cbb9851659542b2eefc5
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-09-02 20:00:53 +0300

    Add Data Validation

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -25,6 +25,9 @@ namespace api.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comments = await _commentRepository.GetAllAsync();
 
             var commentDto = comments.Select(s => s.ToCommentDto());
@@ -33,9 +36,12 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        [Route("{id}")]
+        [Route("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comment = await _commentRepository.GetByIdAsync(id);
 
             if (comment == null)
@@ -46,9 +52,12 @@ namespace api.Controllers
             return Ok(comment.ToCommentDto());
         }
 
-        [HttpPost("{stockId}")]
+        [HttpPost("{stockId:int}")]
         public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             if (!await _stockRepository.StockExists(stockId))
             {
                 return BadRequest("Stock does not exist");
@@ -60,9 +69,12 @@ namespace api.Controllers
             return CreatedAtAction(nameof(GetById), new { id = commentModel.Id }, commentModel.ToCommentDto());
         }
 
-        [HttpPut("{id}")]
+        [HttpPut("{id:int}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var comment = await _commentRepository.UpdateAsync(id, updateDto.ToCommentFromUpdate());
 
             if (comment == null)
@@ -73,9 +85,12 @@ namespace api.Controllers
             return Ok(comment.ToCommentDto());
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete("{id:int}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
             var commentModel = await _commentRepository.DeleteAsync(id);
 
             if (commentModel == null)
--- api/DTOs/Stock/UpdateStockRequestDto.cs
+++ api/DTOs/Stock/UpdateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,28 @@ namespace api.DTOs.Stock
 {
     public class UpdateStockRequestDto
     {
+        [Required]
+        [MaxLength(10, ErrorMessage = "Symbol cannot be over 10 characters long")]
         public string Symbol { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "Company Name cannot be over 10 characters long")]
         public string CompanyName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 10000000000, ErrorMessage = "Price must be between 1$ and 10000000000$")]
         public decimal Purchase { get; set; }
+
+        [Required]
+        [Range(0.001, 100, ErrorMessage = "Dividend must be between 0.001$ and 100$")]
         public decimal LastDiv { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "Industry cannot be over 10 characters long")]
         public string Industry { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, 5000000000, ErrorMessage = "Market Cap should be between 1$ and 5000000000$")]
         public long MarketCap { get; set; }
     }
 }
