a0b36e53fa8630f7f092dc17b92277610e21e237
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-09-05 23:40:46 +0300

    Add POST Portfolio endpoint

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -36,7 +36,36 @@ namespace api.Controllers
             return Ok(userPortfolio);
         }
 
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddPortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var stock = await _stockRepository.GetBySymbolAsync(symbol);
 
+            if (stock == null) return BadRequest("Stock not found");
+
+            var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
+
+            if (userPortfolio.Any(e => e.Symbol.ToLower() == symbol.ToLower())) return BadRequest("Cannot add same stock to portfolio");
+
+            var portfolioModel = new Portfolio
+            {
+                StockId = stock.Id,
+                AppUserId = appUser.Id
+            };
+
+            try
+            {
+                await _portfolioRepository.CreateAsync(portfolioModel);
+                return Created();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Could not create portfolio entry");
+            }
+        }
 
     }
 }
