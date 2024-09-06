bea720122505e5aea22de5eebf83e31944569458
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-09-06 12:42:49 +0300

    Add DELETE Portfolio endpoint

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -67,5 +67,27 @@ namespace api.Controllers
             }
         }
 
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+
+            var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
+
+            var filteredStock = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower());
+
+            if (filteredStock.Count() == 1)
+            {
+                await _portfolioRepository.DeleteAsync(appUser, symbol);
+            }
+            else
+            {
+                return BadRequest("Stock not in your portfolio");
+            }
+
+            return Ok();
+        }
     }
 }
--- api/Repository/PortfolioRepository.cs
+++ api/Repository/PortfolioRepository.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using api.Data;
 using api.Interfaces;
 using api.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
@@ -22,6 +23,18 @@ namespace api.Repository
             await _context.Portfolios.AddAsync(portfolio);
             await _context.SaveChangesAsync();
             return portfolio;
+        }
+
+        public async Task<Portfolio?> DeleteAsync(AppUser appUser, string symbol)
+        {
+            var portfolioModel = await _context.Portfolios.FirstOrDefaultAsync(p => p.Stock.Symbol.ToLower() == symbol.ToLower() && p.AppUserId == appUser.Id);
+            if (portfolioModel == null)
+            {
+                return null;
+            }
+            _context.Portfolios.Remove(portfolioModel);
+            await _context.SaveChangesAsync();
+            return portfolioModel;
         }
 
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)
